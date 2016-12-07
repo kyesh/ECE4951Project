590fd3c926777bdf6d7dac2103f3b4a6a2560235
user <kyesh@umich.edu>
2016-12-06 19:28:37 -0500

    Added Erossion and Dilation, green car mostly removed. Need to make sizing relative instead of absolute

--- src/ECE4951.cpp
+++ src/ECE4951.cpp
@@ -239,7 +239,17 @@ int main(int argc, char** argv )
 
     vector<vector<Point> > squares; //Create list of squares
 
-    cv::Mat img, img_w, img_g;
+    //Element for Erosion and Dialation Operator
+    int erosion_size = 40;
+    Mat elementE = getStructuringElement( MORPH_RECT,
+                                       Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                       Point( erosion_size, erosion_size ) );
+    erosion_size = 5;
+    Mat elementD = getStructuringElement( MORPH_RECT,
+                                       Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                       Point( erosion_size, erosion_size ) );
+
+    cv::Mat img, img_w, img_g, img_gE, img_gD1, img_gD2;
     DIR *dir;
 
     dir = opendir(argv[1]);
@@ -286,20 +296,34 @@ int main(int argc, char** argv )
                 cv::namedWindow("Display Image", cv::WINDOW_NORMAL );
                 cv::namedWindow("White Image", cv::WINDOW_NORMAL );
                 cv::namedWindow("Green Image", cv::WINDOW_NORMAL );
+                cv::namedWindow("Eroded Green Image", cv::WINDOW_NORMAL );
+                cv::namedWindow("Dialated1 Green Image", cv::WINDOW_NORMAL );
+                cv::namedWindow("Dialated2 Green Image", cv::WINDOW_NORMAL );
 
                 CreateNessImage(img, img_w, computeWhiteness);
                 CreateNessImage(img, img_g, computeGreeness);
-		
+                dilate( img_g, img_gD1, elementD );
+         	erode( img_gD1, img_gE, elementE );
+                dilate( img_gE, img_gD2, elementE );
 //                findSquares(img, squares);
 //                drawSquares(img, squares);
 
                 cv::imshow("Display Image", img);
                 cv::imshow("White Image", img_w);
                 cv::imshow("Green Image", img_g);
+                cv::imshow("Eroded Green Image", img_gE);
+                cv::imshow("Dialated1 Green Image", img_gD1);
+                cv::imshow("Dialated2 Green Image", img_gD2);
+
+
                 cout << outdirectory + "SomeQualifier_"  + imgName << endl;
 		cv::imwrite(outdirectory + "Original_"  + imgName, img);
                 cv::imwrite(outdirectory + "White_"  + imgName, img_w);
-                cv::imwrite(outdirectory + "Green_"  + imgName, img_g);            
+                cv::imwrite(outdirectory + "Green_"  + imgName, img_g);
+                cv::imwrite(outdirectory + "GreenE_"  + imgName, img_gE);
+                cv::imwrite(outdirectory + "GreenD1_"  + imgName, img_gD1);
+                cv::imwrite(outdirectory + "GreenD2_"  + imgName, img_gD2);
+            
                 cv::waitKey(0);//Wait until user presses key to continue
             }
         }
